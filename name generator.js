f1284d3a5aadff2fcf91dab2926b6a124b6de13c
Yukon <marcos@linux.Partition>
2022-06-03 20:46:43 -0300

    Refatoração. Apaga variável redundante e adiciona novos comentários.

--- name generator.js
+++ name generator.js
@@ -3,10 +3,9 @@
 const firstNames = ["Roberto", "Rubens", "Pedro", "Enrique"]
 const lastNames = ["Carlos", "da Silva", "de Carvalho", "Magalhães"]
 
-function randomVarArray (array) {
-    var arrayLenght = array.length;
-    return array[Math.floor(Math.random() * arrayLenght)]
-}    debugge
+function randomVarArray (array) {   //returns a random item from the 'array' input
+    return array[Math.floor(Math.random() * array.length)]   //random index number that is larger than 0 and smaller than array.length
+}
 
 function generateName () {
     var fName = randomVarArray(firstNames)
